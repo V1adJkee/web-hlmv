7ad88822b38a5c3699e116192fcbf8a77d46df29
V1adJkee <bass.gx25@gmail.com>
2021-07-30 13:56:09 +0300

    Autoload models if passed GET-param 'mdl'

--- ui/StartScreen.tsx
+++ ui/StartScreen.tsx
@@ -6,32 +6,40 @@ type Props = {
   setFileUrl: (url: string) => void
 }
 
-export const StartScreen = (props: Props) => (
-  <React.Fragment>
-    Try to drop some model here, or{' '}
-    <a
-      href="#"
-      onClick={event => {
-        if (props.selectFile) {
-          props.selectFile(event)
-        }
+export const StartScreen = (props: Props) => {
+  let params = new URL(document.location.href).searchParams
 
-        event.preventDefault()
-      }}
-    >
-      click here
-    </a>{' '}
-    to select a model to upload.
-    <br /> Also, you can{' '}
-    <a
-      href="#"
-      onClick={event => {
-        props.setFileUrl(props.demoFileUrl)
-        event.preventDefault()
-      }}
-    >
-      start demo file viewing
-    </a>
-    .
-  </React.Fragment>
-)
+  if (params.get('mdl') != null) {
+    props.setFileUrl(params.get('mdl') || props.demoFileUrl)
+  }
+
+  return (
+    <React.Fragment>
+      Try to drop some model here, or{' '}
+      <a
+        href="#"
+        onClick={event => {
+          if (props.selectFile) {
+            props.selectFile(event)
+          }
+
+          event.preventDefault()
+        }}
+      >
+        click here
+      </a>{' '}
+      to select a model to upload.
+      <br/> Also, you can{' '}
+      <a
+        href="#"
+        onClick={event => {
+          props.setFileUrl(props.demoFileUrl)
+          event.preventDefault()
+        }}
+      >
+        start demo file viewing
+      </a>
+      .
+    </React.Fragment>
+  )
+}
